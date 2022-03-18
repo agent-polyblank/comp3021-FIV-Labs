decc0910d686699bfe98da897f42a4a014f86a62
psysk6 <psysk6@nottingham.ac.uk>
2022-03-18 13:38:43 +0000

    fixed output dir

--- iris.r
+++ iris.r
@@ -1,6 +1,7 @@
 library(dplyr)
 library(ggplot2)
 library(patchwork)
+library(here)
 #iris is a core dataset and can be loaded like this
 data(iris)
 # can look at the first 5 entries
@@ -27,7 +28,7 @@ head(iris, 5)
 
 
 # write to pdf
-pdf(file="./lab-plots.pdf",width=10,height=5)
+pdf(file="./pdfs/lab-plots.pdf",width=10,height=5)
 
 par(mfrow=c(2,2))
 
@@ -37,9 +38,10 @@ p1 <- ggplot(data=iris,aes(x=Sepal.Width,y=Sepal.Length,colour=Species)) + geom_
 
 p2 <- ggplot(data=iris,aes(x=Petal.Width,y=Petal.Length,colour=Species)) + geom_point() + stat_smooth(method='lm') + labs(x="Petal Width",y="Petal Length",title = "Scatter plot that visualizes the relationship between Petal Width and Petal Length") + theme(plot.title = element_text(size=5))
 
-p3 <-ggplot(data=iris, aes(x=Sepal.Width, y=Species,colour=Species)) + geom_boxplot() + labs(x="Sepal Width",y="Sepal Length",title = "Box plot that visualizes the median and variance of Sepal Lengths across different species;") + theme(plot.title = element_text(size=5))
+p3 <-ggplot(data=iris, aes(x=Sepal.Width, y=Species,colour=Species)) + geom_boxplot() + labs(x="Sepal Width",y="Sepal Length",title = "Box plot that visualizes the median and variance of Sepal Lengths across different species") + theme(plot.title = element_text(size=5))
 
-p4 <- ggplot(data=iris, aes(x=Sepal.Width,colour=Species)) + geom_histogram() + theme(plot.title = element_text(size=5,hjust=TRUE))
+p4 <- ggplot(data=iris, aes(x=Sepal.Width,colour=Species)) + geom_histogram() + theme(plot.title = element_text(size=5,hjust=TRUE)) + labs(x="Sepal Width",y="Sepal Length",title = "Box plot that visualizes the median and variance of Sepal Lengths across different species") + theme(plot.title = element_text(size=5))
+
 
 (p1 | p2) 
 
