ba220b8002a0657c1f53795414dab3badc11b773
psysk6 <psysk6@nottingham.ac.uk>
2022-03-18 13:02:52 +0000

    completed tasks using ggplot

--- iris.r
+++ iris.r
@@ -8,11 +8,35 @@ head(iris, 5)
 
 # select(data,field_1,field_2...,field_N)
 
-
 # 1) Scatter plot that visualizes the relationship between Sepal Width and Sepal Length;
+
+# plot one liner using pipe symbol
 # length_width <- select(iris,Sepal.Length,Sepal.Width) %>% plot(xlab="Sepal Length",ylab="Sepal Width")
 
-length_width <- plot(iris$Sepal.Length ~ iris$Sepal.Width,xlab="Sepal Length",ylab="Sepal Width")
-abline(lm(iris$Sepal.Length ~ iris$Sepal.Width), col = "red")
+# https://bookdown.org/dli/rguide/scatterplots-and-best-fit-lines-two-sets.html
 
+
+
+#R way of doing things
+# sepal_length_width <- plot(iris$Sepal.Width ~ iris$Sepal.Length,xlab="Sepal Width",ylab="Sepal Length")
+# abline(lm(iris$Sepal.Width ~ iris$Sepal.Length), col = "red")
+
+petal_length_width <- plot(iris$Petal.Width ~ iris$Petal.Length,xlab="Petal Width",ylab="Petal Length",)
+abline(lm(iris$Petal.Width ~ iris$Petal.Length), col = "red")
+
+
+# write to pdf
+pdf(file="./lab-plots.pdf")
+
+par(mfrow=c(2,2))
+
+# GGplot2 way of doing things
+
+ggplot(data=iris,aes(x=Sepal.Width,y=Sepal.Length,colour=Species)) + geom_point() + stat_smooth(method='lm') + labs(x="Sepal Width",y="Sepal Length",title = "Scatter plot that visualizes the relationship between Sepal Width and Sepal Length")
+
+ggplot(data=iris, aes(x=Sepal.Width, y=Species,colour=Species)) + geom_boxplot() + labs(x="Sepal Width",y="Sepal Length",title = "Box plot that visualizes the median and variance of Sepal Lengths across different species;")
+
+ggplot(data=iris, aes(x=Sepal.Width,colour=Species)) + geom_histogram()
+
+dev.off()
 
